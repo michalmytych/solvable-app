e9c5826582f868d0154508ad6a4a6b2a033779c7
Mike <michamytych@gmail.com>
2021-08-30 21:34:37 +0200

    Add trait with validation responses in json

---
+++ app/Traits/HasJsonFailedValidationResponse.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Traits;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\ValidationException;
+
+trait HasJsonFailedValidationResponse
+{
+    /**
+     * Handle a failed validation attempt.
+     *
+     * @param Validator $validator
+     * @return void
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        $errors = (new ValidationException($validator))->errors();
+
+        throw new HttpResponseException(
+            response()->json([
+                'errors' => $errors,
+                'message' => 'validation.failed'
+            ], JsonResponse::HTTP_UNPROCESSABLE_ENTITY)
+        );
+    }
+}
