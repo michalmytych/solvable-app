3d682a122e563a609a3a7d9d794fafd998ab3b45
Mike <michamytych@gmail.com>
2021-08-30 21:36:43 +0200

    Add new repository

---
+++ app/Repositories/SolutionRepository.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Problem;
+use App\Models\Solution;
+use Illuminate\Database\Eloquent\Collection;
+use App\Contracts\SolutionRepositoryInterface;
+
+class SolutionRepository implements SolutionRepositoryInterface
+{
+    /**
+     * Get all solutions for provided problem instance.
+     *
+     * @param Problem $problem
+     * @return Collection
+     */
+    public function allByProblem(Problem $problem): Collection
+    {
+        return $problem->solutions;
+    }
+
+    /**
+     * Update provided Solution database record with data.
+     *
+     * @param Solution $solution
+     * @param array $data
+     * @return Solution
+     */
+    public function update(Solution $solution, array $data): Solution
+    {
+        return tap($solution)->update($data);
+    }
+}
