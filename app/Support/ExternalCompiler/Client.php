1521a0b7b09fa31dca3a3fc3c5ec61ad4628ebb7
michalmytych <michamytych@gmail.com>
2021-09-03 02:34:17 +0200

    Add specific exception rethrow with new message at Client

--- app/Support/ExternalCompiler/Client.php
+++ app/Support/ExternalCompiler/Client.php
@@ -6,6 +6,8 @@ use Illuminate\Support\Str;
 use GuzzleHttp\Psr7\Response;
 use Illuminate\Support\Collection;
 use GuzzleHttp\Client as BaseClient;
+use App\Exceptions\CurlError3Exception;
+use GuzzleHttp\Exception\RequestException;
 use App\Exceptions\ExternalServiceInitializationException;
 use App\Contracts\ExternalCompiler\ExternalCompilerClientInterface;
 
@@ -24,20 +26,29 @@ class Client extends BaseClient implements ExternalCompilerClientInterface
     public const REQUESTS_PER_DAY = 200;
 
     /**
-     * @throws ExternalServiceInitializationException
+     * Initialize external service client.
+     *
+     * @throws ExternalServiceInitializationException|CurlError3Exception
      */
     public function init()
     {
         $creditSpent = $this->checkCreditSpent()->get('used');
 
-        if ((int) $creditSpent >= self::REQUESTS_PER_DAY) {
+        if ((int)$creditSpent >= self::REQUESTS_PER_DAY) {
             throw new ExternalServiceInitializationException();
         }
     }
 
+    /**
+     * Post solution code to be executed at external service.
+     *
+     * @param array $data
+     * @return Collection
+     * @throws CurlError3Exception
+     */
     public function postCodeToExecute(array $data): Collection
     {
-        $response = $this->post(
+        $response = $this->templateRequest(fn() => $this->post(
             '/execute',
             [
                 'json' => [
@@ -48,18 +59,31 @@ class Client extends BaseClient implements ExternalCompilerClientInterface
                     'versionIndex' => $data['versionIndex']
                 ],
             ]
-        );
+        ));
 
         return $this->getDecodedResponseData($response);
     }
 
+    /**
+     * Request external service and get amount of request that was made
+     * by client in last 24 hours.
+     *
+     * @return Collection
+     * @throws CurlError3Exception
+     */
     private function checkCreditSpent(): Collection
     {
-        $response = $this->post('/credit-spent');
+        $response = $this->templateRequest(fn() => $this->post('/credit-spent'));
 
         return $this->getDecodedResponseData($response);
     }
 
+    /**
+     * Get response decoded from JSON format.
+     *
+     * @param Response $response
+     * @return Collection
+     */
     private function getDecodedResponseData(Response $response): Collection
     {
         return collect(
@@ -67,8 +91,35 @@ class Client extends BaseClient implements ExternalCompilerClientInterface
         );
     }
 
+    /**
+     * Get name of client's external service.
+     *
+     * @return string
+     */
     public function getName(): string
     {
         return Str::slug($this->name);
+    }
+
+    /**
+     * Wrapper for pure GuzzleHttp requests, with specific exception rethrow.
+     *
+     * @param callable $request
+     * @return mixed
+     * @throws CurlError3Exception
+     */
+    private function templateRequest(callable $request)
+    {
+        try {
+            return $request();
+        } catch (RequestException $exception) {
+            if (Str::of($exception->getMessage())->contains('cURL error 3')) {
+                throw new CurlError3Exception(
+                    'Request error: check provided ' . $this->name . ' service credentials, thrown '
+                );
+            }
+
+            throw $exception;
+        }
     }
 }
