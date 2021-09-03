ebdcc362f3deee1bda438d49b71a97688c0a8fa0
michalmytych <michamytych@gmail.com>
2021-09-03 13:40:43 +0200

    Add create_courses_table and create_groups_table migrations

---
+++ database/migrations/2021_09_03_123646_create_courses_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCoursesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->timestamps();
+            $table->string('name', 96);
+            $table->string('slug', 128);
+            $table->longText('description');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('courses');
+    }
+}
