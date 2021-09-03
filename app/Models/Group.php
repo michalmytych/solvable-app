38f6ffa438386f5e02811db7f4b45770b4ee6fa3
michalmytych <michamytych@gmail.com>
2021-09-03 13:40:17 +0200

    Add course and group models

---
+++ app/Models/Group.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\HasUuid;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Group extends Model
+{
+    use HasUuid, HasFactory;
+
+    protected $fillable = [
+        'name',
+        'code',
+        'description'
+    ];
+
+    protected $hidden = [
+        'id'
+    ];
+
+    /**
+     * Group related to the course.
+     *
+     * @return BelongsToMany
+     */
+    public function courses(): BelongsToMany
+    {
+        return $this->belongsToMany(Course::class, 'group_course');
+    }
+}
