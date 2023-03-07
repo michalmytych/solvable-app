d87e524feb5ec9a44623f41f269d6ccf7df64e9f
mike <michal.mytych@ftggroup.com>
2023-03-07 19:44:43 +0100

    Update docs

--- resources/views/docs/components-showcase.blade.php
+++ resources/views/docs/components-showcase.blade.php
@@ -134,11 +134,6 @@
                 <x-select name="test"/>
             </x-card>
 
-            <!-- Verbose checkbox component -->
-            <x-card header="Verbose checkbox" id="verbose_checkbox_component">
-                <x-verbose-checkbox name="test"/>
-            </x-card>
-
             <!-- Form component -->
             <x-card header="Form" id="form_component">
                 <x-form method="GET" action="#">
